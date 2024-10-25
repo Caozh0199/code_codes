82b86dbc85e2b6508cef6609a5ee0b9f9c45ed9f
Caozh0199 <1053236458@qq.com>
2024-10-25 15:05:35 +0800

    test

--- Transaction_logistics/vue/src/components/Footer.vue
+++ Transaction_logistics/vue/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 80px; padding: 10px 0; line-height: 30px; color: #666; text-align: center">
-    <div>二手交易物流管理平台</div>
+    <div>二手交易物流管理平台 v1.0</div>
   </div>
 </template>
 
